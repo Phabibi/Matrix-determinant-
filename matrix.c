746f5ffd20df14c064bab9e8fdf47fa17ca21b30
phabibi <phabibi@asb9838nu-c12.csil.sfu.ca>
2018-07-18 17:38:08 -0700

    threads are now working boyz

--- matrix.c
+++ matrix.c
@@ -1,14 +1,24 @@
-#include <stdlib.h>
+
+/*
+- Description: Matrix Determenant
+- Author: Parsa Habibi
+- Date : July 17th 2018
+*/
+
+
 #include <stdio.h>
-
-
-
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <stdbool.h>
+int n;
 void random_matrix ()
 {
-  int n;
   scanf("%d", &n);
   printf(" got n = %d\n", n );
   int matrix[n][n];
+  printf("[");
 
   for(int i = 0 ; i < n ; i++)
   {
@@ -16,18 +26,58 @@ void random_matrix ()
 
     for(int j = 0; j < n; j++)
     {
-      matrix[i][j] = (rand()% (100 - 2 + 1) + 2) ;
+      matrix[i][j] = (rand()% (10 + 10) - 10) ;
       printf("%d ", matrix[i][j]);
     }
   }
-  printf("\n");
+
+  printf("] \n");
 
   return;
 
 }
 
+int Determenant(int *arr)
+{
+    for(int i = 0 ; i < n ; i++)
+    {
+      for(int j = 0; j < i ; j++)
+      {
+        arr[i][j] *= arr
+      }
+
+    }
+}
+
+void *prompt(void* arg)
+{
+  int id = *((int *) arg);
+  printf("the thread id %d\n", id );
+}
+
 void main ()
 {
   random_matrix();
+
+  pthread_t thread_id[n];
+
+  for(int i = 0; i < n ; i++)
+{
+  int *arg = malloc(sizeof(*arg));
+  if ( arg == NULL ) {
+      fprintf(stderr, "Couldn't allocate memory for thread arg.\n");
+      exit(EXIT_FAILURE);
+  }
+
+  *arg = i;
+  pthread_create(&thread_id[i],NULL,prompt,arg);
+}
+
+for(int i = 0 ; i < n ; i++)
+  {
+    pthread_join(thread_id[i], NULL);
+  }
+
+
   return;
 }
